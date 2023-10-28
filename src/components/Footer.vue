a212a6ff3b349b67e18f4fabbba16b6286d08b7e
Tverous <cwtang@vt.edu>
2023-10-28 17:57:16 -0400

    vite w/ vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+    <a href='https://www.vt.edu/' target="_blank">
+        <img src="/src/assets/vt_logo.svg" class="logo" alt="Virginia Tech logo" />
+    </a>
+</template>
+
+<style scoped></style>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/kg-visualize/',
+    base: '/kg-visualize/',
+    plugins: [vue()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                network2d: resolve(__dirname, 'pages', 'network2d.html'),
+                network3d: resolve(__dirname, 'pages', 'network3d.html'),
+            },
+        }
+    }
 })
