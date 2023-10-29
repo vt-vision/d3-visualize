74cf3db322f80f1be5bd47167f72184d638e51e6
Tverous <cwtang@vt.edu>
2023-10-29 15:48:48 -0400

    add self-signed ssl

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import vue from '@vitejs/plugin-vue'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [basicSsl()],
     build: {
         rollupOptions: {
             input: {
