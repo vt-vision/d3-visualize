e993ef3883293379627ea4a90abe00b231d60faf
Tverous <Tverous@protonmail.com>
2023-10-30 02:49:24 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-    plugins: [basicSsl()],
+    base: '/d3-visualize/',
+    plugins: [],
     build: {
         rollupOptions: {
             input: {
